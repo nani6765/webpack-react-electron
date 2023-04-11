46e924aa7fcc9a4720e6db693cacc14233d87753
이재원 <nani6765@doublemedia.co.kr>
2023-04-11 16:13:34 +0900

    ipc ping-pong

--- public/electron.js
+++ public/electron.js
@@ -1,39 +1,90 @@
-const {app,BrowserWindow} = require('electron');
- 
-const path = require('path')
-const isDev = require('electron-is-dev')
- 
-const remote = require('@electron/remote/main')
-remote.initialize()
- 
+/* eslint-disable no-unused-vars */
+// Module to control the application lifecycle and the native browser window.
+const { app, BrowserWindow, protocol, ipcMain } = require("electron");
+const path = require("path");
+const url = require("url");
+
+// Create the native browser window.
 function createWindow() {
-    const win = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            nodeIntegration: true,
-            enableRemoteModule: true,
-            contextIsolation: false
-        }
-    })
- 
-    win.loadURL(
-        isDev
-          ? 'http://localhost:3000'
-          : `file://${path.join(__dirname, '../build/index.html')}`
-      )
- 
-    remote.enable(win.webContents);
+  const mainWindow = new BrowserWindow({  
+    width: 800,  
+    height: 600,  
+    webPreferences: {  
+      nodeIntegration: true, // is default value after Electron v5
+      contextIsolation : false,   // protect against prototype pollution
+      // enableRemoteModule: false, // turn off remote
+    }  
+  })  
+
+  // In production, set the initial browser path to the local bundle generated
+  // by the Create React App build process.
+  // In development, set it to localhost to allow live/hot-reloading.
+  const appURL = app.isPackaged
+    ? url.format({
+        pathname: path.join(__dirname, "index.html"),
+        protocol: "file:",
+        slashes: true,
+      })
+    : "http://localhost:3000";
+  mainWindow.loadURL(appURL);
+
+  // Automatically open Chrome's DevTools in development mode.
+  if (!app.isPackaged) {
+    mainWindow.webContents.openDevTools();
+  }
 }
- 
-app.on('ready', createWindow)
- 
-app.on('window-all-closed', function() {
-    if(process.platform !== 'darwin') {
-        app.quit()
+
+// Setup a local proxy to adjust the paths of requested files when loading
+// them from the local production bundle (e.g.: local fonts, etc...).
+function setupLocalFilesNormalizerProxy() {
+  protocol.registerHttpProtocol(
+    "file",
+    (request, callback) => {
+      const url = request.url.substr(8);
+      callback({ path: path.normalize(`${__dirname}/${url}`) });
+    },
+    (error) => {
+      if (error) console.error("Failed to register protocol");
     }
-})
- 
-app.on('activate', function() {
-    if(BrowserWindow.getAllWindows().length === 0) createWindow()
-})
+  );
+}
+// ipc Start
+ipcMain.on("ping", (event, arg)=>{ 
+  console.log('Main received a ping!!!'); 
+}) 
+// ipc End
+
+
+app.whenReady().then(() => {
+  createWindow();
+  setupLocalFilesNormalizerProxy();
+
+  app.on("activate", function () {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+// Quit when all windows are closed, except on macOS.
+// There, it's common for applications and their menu bar to stay active until
+// the user quits  explicitly with Cmd + Q.
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+// If your app has no need to navigate or only needs to navigate to known pages,
+// it is a good idea to limit navigation outright to that known scope,
+// disallowing any other kinds of navigation.
+app.on("web-contents-created", (event, contents) => {
+  contents.on("will-navigate", (event, navigationUrl) => {
+    // const parsedUrl = new URL(navigationUrl);
+    // if (!allowedNavigationDestinations.includes(parsedUrl.origin)) {
+    //   event.preventDefault();
+    // }
+  });
+});
--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
-function App() {
-  return <div>Appasdaaasdasdasd</div>;
-}
-
+function App() { 
+  const { ipcRenderer } = window.require("electron"); 
+  const sendMail = () => { 
+    ipcRenderer.send("ping", 'send'); 
+  } 
+  return ( 
+    <div className="App"> 
+      <header className="App-header"> 
+        <p> 
+          Hello world!!! 
+        </p> 
+        <a 
+          className="App-link" 
+          href="https://reactjs.org" 
+          target="_blank" 
+          rel="noopener noreferrer" 
+        > 
+          Learn React 
+        </a> 
+        <button onClick={ sendMail }>Send Mail</button> 
+      </header> 
+    </div> 
+  ); 
+} 
 export default App;
